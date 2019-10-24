6698a7b419a1ebd6ef12e4d82dd2f9524d983966
Jeremy Xue <jeremyxue@gmail.com>
2019-10-24 14:00:25 -0400

    Addded additional endpoints

--- HomeCooked/homecooked_firebase.playground/Contents.swift
+++ HomeCooked/homecooked_firebase.playground/Contents.swift
@@ -2,12 +2,32 @@ import UIKit
 import SwiftyJSON
 import XCPlayground
 
-let fbURL: NSURL = NSURL(string: "https://firestore.googleapis.com/v1/projects/homecooked-b0581/databases/(default)/documents/chef/GJFmRADeY9MJCQ4AUTpC")!
+let chefURL: NSURL = NSURL(string: "https://firestore.googleapis.com/v1/projects/homecooked-b0581/databases/(default)/documents/chef/")!
 
-let data = NSData(contentsOf: fbURL as URL)!
+let chefdata = NSData(contentsOf: chefURL as URL)!
 
-let swiftyjson = try JSON(data: data as Data)
+let chefswiftyjson = try JSON(data: chefdata as Data)
 
-let chef = swiftyjson["fields"]
+let chef = chefswiftyjson["documents"]
   
 print(chef)
+
+let guestURL: NSURL = NSURL(string: "https://firestore.googleapis.com/v1/projects/homecooked-b0581/databases/(default)/documents/guest/")!
+
+let guestdata = NSData(contentsOf: guestURL as URL)!
+
+let guestswiftyjson = try JSON(data: guestdata as Data)
+
+let guest = guestswiftyjson["documents"]
+  
+print(guest)
+
+let reservationURL: NSURL = NSURL(string: "https://firestore.googleapis.com/v1/projects/homecooked-b0581/databases/(default)/documents/guest/")!
+
+let reservationdata = NSData(contentsOf: reservationURL as URL)!
+
+let reservationswiftyjson = try JSON(data: reservationdata as Data)
+
+let reservation = reservationswiftyjson["documents"]
+  
+print(reservation)
