9fee000c19c8e590ccc42c058d35b303a1ab97a1
Albert Yuan <albear@Alberts-MBP.fios-router.home>
2019-10-30 20:25:06 -0400

    re-init with swiftui

---
+++ HomeCooked/HomeCooked/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeCooked
+//
+//  Created by Albert Yuan on 10/30/19.
+//  Copyright © 2019 67442. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
