f3b21f8dd805774fd0e3b0af303c7f3f866980dc
Jeremy Xue <jeremyxue@gmail.com>
2019-11-07 14:30:16 -0500

    added post reservation

--- HomeCooked/HomeCooked/Views/Reservation/ReservationViewModel.swift
+++ HomeCooked/HomeCooked/Views/Reservation/ReservationViewModel.swift
@@ -13,10 +13,32 @@ import FirebaseFirestore
 
 class ReservationViewModel {
   let reservation: Reservation
+  var db: Firestore!
 
   init(reservation: Reservation) {
     self.reservation = reservation
+    let settings = FirestoreSettings()
+
+    Firestore.firestore().settings = settings
+    // [END setup]
+    db = Firestore.firestore()
   }
   
-  func postReservation(reservation: Reservation)
+  func postReservation(reservation: Reservation) {
+    let documentId = String(reservation.id)
+    db.collection("reservation").document(documentId).setData([
+      "id": reservation.id,
+      "guest_count": reservation.guest_count,
+      "guest_id": reservation.guest_id,
+      "payment_info": "This is a credit card",
+      "meal_id": reservation.meal_id,
+      "reservation_time": "Now",
+    ]) { err in
+        if let err = err {
+            print("Error writing document: \(err)")
+        } else {
+            print("Document successfully written!")
+        }
+    }
+  }
 }
