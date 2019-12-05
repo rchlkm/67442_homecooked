e08e174007f88bfb497d91211f1ef53e7a6c2fac
Rachel Kim <rachelkim1205@gmail.com>
2019-12-05 04:34:52 -0500

    added allergen, cuisine filters

--- HomeCooked/HomeCooked/Views/Common/OptionButton.swift
+++ HomeCooked/HomeCooked/Views/Common/OptionButton.swift
@@ -9,27 +9,51 @@
 import SwiftUI
 
 struct OptionButton: View {
-    @State private var didTap: Bool = false
+//    @State private var didTap: Bool = false
     let text: String
-    init(_ text: String) {
+    var didTap: Bool
+    init(_ text: String, didTap: Bool = false) {
         self.text = text
+        self.didTap = didTap
     }
     var body: some View {
-        Button(action: {
-            self.didTap.toggle()
-        }) {
-            Text(self.text)
-                .font(.footnote)
-                .foregroundColor(didTap ? Color.white : Color.black)
-                .padding(8)
-                .background(didTap ? OrangeColor : Color.white)
-                .clipShape(RoundedRectangle(cornerRadius: corner_radius, style: .continuous))
-                .overlay(RoundedRectangle(cornerRadius: corner_radius, style: .continuous)
-                    .stroke(OrangeColor, lineWidth: 1))
-                .padding(5)
-        }
+         Text(self.text)
+            .font(.footnote)
+            .foregroundColor(didTap ? Color.white : Color.black)
+            .padding(8)
+            .background(didTap ? OrangeColor : Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: corner_radius, style: .continuous))
+            .overlay(RoundedRectangle(cornerRadius: corner_radius, style: .continuous)
+                .stroke(OrangeColor, lineWidth: 1))
+            .padding(5)
     }
 }
+
+
+//struct OptionButton: View {
+////    @State private var didTap: Bool = false
+//    let text: String
+//    @State var didTap: Bool
+//    init(_ text: String, didTap: Bool = false) {
+//        self.text = text
+//        self.didTap = didTap
+//    }
+//    var body: some View {
+//        Button(action: {
+//            self.didTap.toggle()
+//        }) {
+//            Text(self.text)
+//                .font(.footnote)
+//                .foregroundColor(didTap ? Color.white : Color.black)
+//                .padding(8)
+//                .background(didTap ? OrangeColor : Color.white)
+//                .clipShape(RoundedRectangle(cornerRadius: corner_radius, style: .continuous))
+//                .overlay(RoundedRectangle(cornerRadius: corner_radius, style: .continuous)
+//                    .stroke(OrangeColor, lineWidth: 1))
+//                .padding(5)
+//        }
+//    }
+//}
 
 struct OptionButton_Previews: PreviewProvider {
     static var previews: some View {
--- HomeCooked/HomeCooked/Views/Search/SearchView.swift
+++ HomeCooked/HomeCooked/Views/Search/SearchView.swift
@@ -8,21 +8,18 @@
 
 import SwiftUI
 
-let search_filters = ["Vegetarian", "Vegan", "Halal"]
-
 struct SearchView: View {
     @State var filterIsPresent = false
     @State var search_city: String = ""
     @State var guest_count: Int = 2
     @State var search_date = Date()
     @ObservedObject var vm : SearchViewModel
+    /*
+     let vm = SearchViewModel()
+     @State var search_results: [Meal] = [Meal]()
+     @State var updated: Bool = false
+     */
     
-/*
-    let vm = SearchViewModel()
-    @State var search_results: [Meal] = [Meal]()
-    @State var updated: Bool = false
-  */
-
     @State private var didTap: Bool = false
     
     init(){
@@ -47,21 +44,21 @@ struct SearchView: View {
                 }
             }
         }.border(Color.blue)
-        .padding(.leading,20).padding(.trailing,20)
-        .navigationBarTitle("Search")
-        .navigationBarBackButtonHidden(true)
-        .navigationBarHidden(true)
+            .padding(.leading,20).padding(.trailing,20)
+            .navigationBarTitle("Search")
+            .navigationBarBackButtonHidden(true)
+            .navigationBarHidden(true)
     }
     
     func submitSearch() {
-/*
-      let search_params = SearchParams(city: self.search_city, year: self.search_date.year, month: self.search_date.month, day: self.search_date.day, max_guest_count: self.max_guest_count)
-        // do something here with results
-        self.search_results = vm.search(params: search_params)
-        print(self.search_results)
-        self.updated = !self.updated
-        print(self.updated)
-*/
+        /*
+         let search_params = SearchParams(city: self.search_city, year: self.search_date.year, month: self.search_date.month, day: self.search_date.day, max_guest_count: self.max_guest_count)
+         // do something here with results
+         self.search_results = vm.search(params: search_params)
+         print(self.search_results)
+         self.updated = !self.updated
+         print(self.updated)
+         */
         let search_params = SearchParams(city: self.search_city, year: self.search_date.year, month: self.search_date.month, day: self.search_date.day, max_guest_count: self.guest_count)
         
         self.vm.search(params: search_params)
@@ -134,21 +131,124 @@ struct SearchView: View {
                 Text("")
             }.padding(.bottom, 20)
             
-            SectionTitle("Dietary").padding(.bottom)
-            HStack {
-                ForEach(search_filters, id: \.self) { filter in
-                    OptionButton(filter)
-                }
-            }.padding(.bottom, 40)
+            //            dietaryFilters.padding(.bottom, 20)
+            cuisineFilters.padding(.bottom, 20)
+            allergensFilters.padding(.bottom, 40)
             Button(action: {
                 self.filterIsPresent.toggle()
             }) { OrangeButton("Apply") }
             
         }.padding()
-            //.border(Color.blue)
     }
+    
+    
+    @State var filter_vegetarian = false
+    @State var filter_vegan = false
+    @State var filter_halal = false
+    let search_filters = ["Vegetarian", "Vegan", "Halal"]
+    var dietaryFilters: some View {
+        VStack {
+            SectionTitle("Dietary").padding(.bottom)
+            HStack {
+                ForEach(search_filters, id: \.self) { filter in
+                    Button(action: {
+                        switch filter {
+                        case "Vegetarian": self.filter_vegetarian.toggle()
+                        case "Vegan": self.filter_vegan.toggle()
+                        case "Halal": self.filter_halal.toggle()
+                        default: self.filter_vegetarian.toggle()
+                        }
+                    }) {
+                        if filter == "Vegetarian" {
+                            OptionButton(filter, didTap: self.filter_vegetarian)
+                        } else if filter == "Vegan" {
+                            OptionButton(filter, didTap: self.filter_vegan)
+                        } else if filter == "Halal" {
+                            OptionButton(filter, didTap: self.filter_halal)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    @State var cuisine_asian = false
+    @State var cuisine_american = false
+    @State var cuisine_italian = false
+    @State var cuisine_mexican = false
+    @State var cuisine_mediterranean = false
+    let cuisine_filters = ["Asian", "American", "Italian", "Mexican", "Mediterranean"]
+    var cuisineFilters: some View {
+        VStack {
+            SectionTitle("Cuisine").padding(.bottom)
+            HStack {
+                ForEach(cuisine_filters, id: \.self) { filter in
+                    Button(action: {
+                        switch filter {
+                        case "Asian": self.cuisine_asian.toggle()
+                        case "American": self.cuisine_american.toggle()
+                        case "Italian": self.cuisine_italian.toggle()
+                        case "Mexican": self.cuisine_mexican.toggle()
+                        case "Mediterranean": self.cuisine_mediterranean.toggle()
+                        default: self.cuisine_mediterranean.toggle()
+                        }
+                    }) {
+                        if filter == "Asian" {
+                            OptionButton(filter, didTap: self.cuisine_asian)
+                        } else if filter == "American" {
+                            OptionButton(filter, didTap: self.cuisine_american)
+                        } else if filter == "Italian" {
+                            OptionButton(filter, didTap: self.cuisine_italian)
+                        } else if filter == "Mexican" {
+                            OptionButton(filter, didTap: self.cuisine_mexican)
+                        } else if filter == "Mediterranean" {
+                            OptionButton(filter, didTap: self.cuisine_mediterranean)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    @State var allergens_nuts = false
+    @State var allergens_shellfish = false
+    @State var allergens_dairy = false
+    @State var allergens_fish = false
+    @State var allergens_soy = false
+    let allergen_filters = ["Nuts", "Shellfish", "Dairy", "Fish", "Soy"]
+    var allergensFilters: some View {
+        VStack {
+            SectionTitle("Allergens").padding(.bottom)
+            HStack {
+                ForEach(allergen_filters, id: \.self) { filter in
+                    Button(action: {
+                        switch filter {
+                            case "Nuts": self.allergens_nuts.toggle()
+                            case "Shellfish": self.allergens_shellfish.toggle()
+                            case "Dairy": self.allergens_dairy.toggle()
+                            case "Fish": self.allergens_fish.toggle()
+                            case "Soy": self.allergens_soy.toggle()
+                        default: self.allergens_nuts.toggle()
+                        }
+                    }) {
+                        if filter == "Nuts" {
+                            OptionButton(filter, didTap: self.allergens_nuts)
+                        } else if filter == "Shellfish" {
+                            OptionButton(filter, didTap: self.allergens_shellfish)
+                        } else if filter == "Dairy" {
+                            OptionButton(filter, didTap: self.allergens_dairy)
+                        } else if filter == "Fish" {
+                            OptionButton(filter, didTap: self.allergens_fish)
+                        } else if filter == "Soy" {
+                            OptionButton(filter, didTap: self.allergens_soy)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
 }
-
 
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
